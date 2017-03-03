e0ed761b630b0840ee738f5530f285b21afa8191
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2017-03-03 14:02:23 +0300

    test

--- rl/config.py
+++ rl/config.py
@@ -2,7 +2,7 @@ class Config(object):
     _lambda = 1.0
     gamma = 0.0
     buffer_length = 100
-    learning_rate = 0.001
+    learning_rate = 0.0001
     enthropy_weight = 0.0001
 
 _config = Config()
