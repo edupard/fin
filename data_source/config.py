03b0733d2e4cdf50dd3c7a877c6cc81e77fe520c
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2017-03-09 15:57:37 +0300

    1h

--- data_source/config.py
+++ data_source/config.py
@@ -5,7 +5,7 @@ class Config(object):
     # fin environment
     ticker = 'QO'
     # ticker = 'EXP'
-    bar_min = 4 * 60
+    bar_min = 1 * 60
     # bar_min = 30
     switch_off_zero_bars = True
 
