c6f8c11b10d34a5a87feeda216ebc42c1c5282b3
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2017-03-29 15:27:32 +0300

    leha test adjusted

--- config.py
+++ config.py
@@ -125,7 +125,7 @@ class Config(object):
     num_global_steps = 20e8
     algo_modification = True
     _lambda = 1.0
-    gamma = 1.0
+    gamma = 0.95
     buffer_length = 100
     fwd_buffer_length = buffer_length if algo_modification else 0
     keep_prob = 0.5
