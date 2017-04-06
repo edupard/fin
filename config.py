bad897f79c4588f8487def21bfdbcf1ce784378b
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2017-04-06 17:07:25 +0300

    test more complicated model

--- config.py
+++ config.py
@@ -181,8 +181,11 @@ class Config(object):
     # conv_layers_1d = [(3, 2, 64), (3, 2, 32), (3, 2, 16), (3, 2, 8), (3, 2, 4), (4, 4, 2)]
     # rnn_1d_size = 2
     # even more simple model
-    conv_layers_1d = [(3, 2, 16), (3, 2, 8), (3, 2, 4), (3, 2, 2), (3, 2, 1)]
-    rnn_1d_size = 2
+    # conv_layers_1d = [(3, 2, 16), (3, 2, 8), (3, 2, 4), (3, 2, 2), (3, 2, 1)]
+    # rnn_1d_size = 2
+    # leha model - 1h 6000 bars
+    conv_layers_1d = [(3, 2, 32), (3, 2, 16), (3, 2, 8)]
+    rnn_1d_size = 13
 
     max_grad_norm = 40.0
     propogate_position_to_rnn = False
