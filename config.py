b670e657ff3e61b8d66246a0f0221664d2bc1ff4
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2017-04-06 10:21:04 +0300

    move to 15 min

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ class Config(object):
     # bar_min = 24 * 60
     # Brent
     ticker = 'QO'
-    bar_min = 4 * 60  # 5  # 60  # 15
+    bar_min = 15  # 4 * 60  # 5  # 60  # 15
     # Experiments
     # ticker = 'EXP'
     # bar_min = 30
@@ -115,7 +115,7 @@ class Config(object):
     mode = Mode.TRAIN
     train_length = 3000  # 12000 #6000 #12 * 6000  # 6000  # 12 * 6000  # 3000  # 6000 * 4
     train_episode_length = train_length
-    retrain_interval = train_episode_length  # 1000 #train_episode_length  # 2100  # train_episode_length
+    retrain_interval = train_episode_length // 6  # 1000 #train_episode_length  # 2100  # train_episode_length
     train_seed = 0
 
     # Learning parameters
@@ -144,17 +144,17 @@ class Config(object):
     #                   (3, 3, 2, 2, 32), (3, 3, 3, 3, 32)]
     # rnn_2d_size = 64
     # superfit
-    # conv_layers_2d = [(3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32),
-    #                   (3, 3, 2, 2, 32)]
-    # rnn_2d_size = 256
+    conv_layers_2d = [(3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32), (3, 3, 2, 2, 32),
+                      (3, 3, 2, 2, 32)]
+    rnn_2d_size = 256
     # less than superfit
     # conv_layers_2d = [(3, 3, 2, 2, 4), (3, 3, 2, 2, 4), (3, 3, 2, 2, 4), (3, 3, 2, 2, 4), (3, 3, 2, 2, 4),
     #                   (3, 3, 2, 2, 4)]
     # rnn_2d_size = 36
     # even more less
-    conv_layers_2d = [(3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2),
-                      (3, 3, 2, 2, 2)]
-    rnn_2d_size = 18
+    # conv_layers_2d = [(3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2), (3, 3, 2, 2, 2),
+    #                   (3, 3, 2, 2, 2)]
+    # rnn_2d_size = 18
 
 
     # conv_layers_1d = [(3, 2, 5), (3, 2, 5), (3, 2, 5), (3, 2, 5), (3, 2, 5)]
